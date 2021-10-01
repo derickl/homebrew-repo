b4fd289d2e4857418f50781d38ca5b697535eb86
Derick Lung'aho <83162+derickl@users.noreply.github.com>
2021-10-02 01:52:17 +0300

    Add basez

---
+++ Formula/basez.rb
@@ -0,0 +1,21 @@
+class Basez < Formula
+  desc "basez - base 16/32/64 encode/decode data to standard output"
+  homepage "http://www.quarkline.net/basez/"
+  url "http://www.quarkline.net/basez/download/basez-1.6.2.tar.gz"
+  sha256 "2a9f821488791c2763ef0120c75c43dc83dd16567b7c416f30331889fd598937"
+  license "GPL-3.0"
+  
+  depends_on "help2man" => :build
+  conflicts_with "coreutils", :because => "both install base32 binaries"
+  
+  def install
+    ENV.deparallelize
+    system "./configure", *std_configure_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "basez --help"
+  end
+end
